3729623de4ab2a73e7f21d2513e2d35a1843164a
Ktoscoszjadl <bartosz.ogorek21@gmail.com>
2025-03-13 12:09:02 +0100

    backend with some api

---
+++ backend/api/views.py
@@ -0,0 +1,205 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.db.models import Sum
+# Restframework
+from rest_framework import status
+from rest_framework.decorators import api_view, APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from datetime import datetime
+
+# Others
+import json
+import random
+
+# Custom Imports
+from api import serializer as api_serializer
+from api import models as api_models
+
+class MyTokenObtainPairView(TokenObtainPairView):
+   
+    serializer_class = api_serializer.MyTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    
+    queryset = api_models.User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = api_serializer.RegisterSerializer
+
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = api_serializer.ProfileSerializer
+
+    def get_object(self):
+        user_id = self.kwargs['user_id']
+
+        user = api_models.User.objects.get(id=user_id)
+        profile = api_models.Profile.objects.get(user=user)
+        return profile
+    
+
+def generate_numeric_otp(length=7): 
+        otp = ''.join([str(random.randint(0, 9)) for _ in range(length)])
+        return otp
+
+
+######################################### API VIEWS ##########################################################
+class CategoryListAPIView(generics.ListAPIView):
+    serializer_class = api_serializer.CategorySerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        return api_models.Category.objects.all()
+
+class PostCategoryListAPIView(generics.ListAPIView):
+    serializer_class = api_serializer.PostSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        category_slug = self.kwargs['category_slug'] 
+        category = api_models.Category.objects.get(slug=category_slug)
+        return api_models.Post.objects.filter(category=category, status="Active")
+
+class PostListAPIView(generics.ListAPIView):
+    serializer_class = api_serializer.PostSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        return api_models.Post.objects.all()
+    
+class PostDetailAPIView(generics.RetrieveAPIView):
+    serializer_class = api_serializer.PostSerializer
+    permission_classes = [AllowAny]
+
+    def get_object(self):
+        slug = self.kwargs['slug']
+        post = api_models.Post.objects.get(slug=slug, status="Active")
+        post.view += 1
+        post.save()
+        return post
+        
+class LikePostAPIView(APIView):
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'user_id': openapi.Schema(type=openapi.TYPE_INTEGER),
+                'post_id': openapi.Schema(type=openapi.TYPE_STRING),
+            },
+        ),
+    )
+    def post(self, request):
+        user_id = request.data['user_id']
+        post_id = request.data['post_id']
+
+        user = api_models.User.objects.get(id=user_id)
+        post = api_models.Post.objects.get(id=post_id)
+
+        # Check if post has already been liked by this user
+        if user in post.likes.all():
+            # If liked, unlike post
+            post.likes.remove(user)
+            return Response({"message": "Post Disliked"}, status=status.HTTP_200_OK)
+        else:
+            # If post hasn't been liked, like the post by adding user to set of poeple who have liked the post
+            post.likes.add(user)
+            
+            # Create Notification for Author
+            api_models.Notification.objects.create(
+                user=post.user,
+                post=post,
+                type="Like",
+            )
+            return Response({"message": "Post Liked"}, status=status.HTTP_201_CREATED)
+        
+class PostCommentAPIView(APIView):
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'post_id': openapi.Schema(type=openapi.TYPE_INTEGER),
+                'name': openapi.Schema(type=openapi.TYPE_STRING),
+                'email': openapi.Schema(type=openapi.TYPE_STRING),
+                'comment': openapi.Schema(type=openapi.TYPE_STRING),
+            },
+        ),
+    )
+    def post(self, request):
+        # Get data from request.data (frontend)
+        post_id = request.data['post_id']
+        name = request.data['name']
+        email = request.data['email']
+        comment = request.data['comment']
+
+        post = api_models.Post.objects.get(id=post_id)
+
+        # Create Comment
+        api_models.Comment.objects.create(
+            post=post,
+            name=name,
+            email=email,
+            comment=comment,
+        )
+
+        # Notification
+        api_models.Notification.objects.create(
+            user=post.user,
+            post=post,
+            type="Comment",
+        )
+
+        # Return response back to the frontend
+        return Response({"message": "Commented Sent"}, status=status.HTTP_201_CREATED)
+ 
+class BookmarkPostAPIView(APIView):
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'user_id': openapi.Schema(type=openapi.TYPE_INTEGER),
+                'post_id': openapi.Schema(type=openapi.TYPE_STRING),
+            },
+        ),
+    )
+    
+    def post(self, request):
+        user_id = request.data['user_id']
+        post_id = request.data['post_id']
+
+        user = api_models.User.objects.get(id=user_id)
+        post = api_models.Post.objects.get(id=post_id)
+
+        bookmark = api_models.Bookmark.objects.filter(post=post, user=user).first()
+        if bookmark:
+            # Remove post from bookmark
+            bookmark.delete()
+            return Response({"message": "Post Un-Bookmarked"}, status=status.HTTP_200_OK)
+        else:
+            api_models.Bookmark.objects.create(
+                user=user,
+                post=post
+            )
+
+            # Notification
+            api_models.Notification.objects.create(
+                user=post.user,
+                post=post,
+                type="Bookmark",
+            )
+            return Response({"message": "Post Bookmarked"}, status=status.HTTP_201_CREATED)
+
